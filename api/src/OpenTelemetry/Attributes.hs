5e5612714e00742a74237613fa028a579f593d25
Ian-Woo Kim <ianwookim@gmail.com>
2023-03-22 18:17:42 -0700

    foldl -> foldl'

--- api/src/OpenTelemetry/Attributes.hs
+++ api/src/OpenTelemetry/Attributes.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE DeriveGeneric #-}
@@ -42,6 +43,7 @@ module OpenTelemetry.Attributes
   , unsafeMergeAttributesIgnoringLimits 
   ) where
 import Data.Int ( Int64 )
+import Data.List (foldl')
 import Data.Text ( Text )
 import qualified Data.HashMap.Strict as H
 import qualified Data.Text as T
@@ -73,7 +75,7 @@ emptyAttributes :: Attributes
 emptyAttributes = Attributes mempty 0 0
 
 addAttribute :: ToAttribute a => AttributeLimits -> Attributes -> Text -> a -> Attributes
-addAttribute AttributeLimits{..} Attributes{..} k v = case attributeCountLimit of
+addAttribute AttributeLimits{..} Attributes{..} !k !v = case attributeCountLimit of
   Nothing -> Attributes newAttrs newCount attributesDropped
   Just limit_ -> if newCount > limit_
     then Attributes attributes attributesCount (attributesDropped + 1)
@@ -97,7 +99,7 @@ addAttribute AttributeLimits{..} Attributes{..} k v = case attributeCountLimit o
 
 addAttributes :: ToAttribute a => AttributeLimits -> Attributes -> [(Text, a)] -> Attributes
 -- TODO, this could be done more efficiently
-addAttributes limits = foldl (\attrs' (k, v) -> addAttribute limits attrs' k v)
+addAttributes limits = foldl' (\(!attrs') (!k, !v) -> addAttribute limits attrs' k v)
 {-# INLINE addAttributes #-}
 
 getAttributes :: Attributes -> (Int, H.HashMap Text Attribute)
